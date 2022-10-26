9bfc820b0ffa12ba33aceefd51c6b274d60f39ac
Edwin_Silvestre <edwinsilvestre0101@gmail.com>
2022-10-26 18:40:30 -0400

    He podido profundizar las enumeraciones y entenderlas

--- Program.cs
+++ Program.cs
@@ -1,28 +1,42 @@
 using System;
-using Codevelp;
 
-namespace Codevelp
+namespace Codevelp 
 {
+    public enum Subyasent 
+    {
+        one,
+        two,
+        tree,
+        four,
+        five
+    }
     public class Initial : object 
     {
-        public void Questions ()
+        private Subyasent numbers;
+        public Subyasent Number 
         {
-            Console.Write("Hello!");
-            Console.ReadLine();
-            Console.Write("What's your name?: ");
-            string name = Convert.ToString(Console.ReadLine());
-            Console.Write("What's your age old?: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("hello {0}, your age old is {1}. nice to meet you...", name, age);
-            Console.ReadLine();
-            Console.WriteLine("Goodbye {0}", name);
-            Console.Read();
-
+            get { return this.numbers; }
+            set { this.numbers = value; }
         }
-        internal static void Main (string[] Arguments)
+        public static void Main (string[] Arguments)
         {
-            Initial start = new Initial();
-            start.Questions();
+            Initial Beginning = new Initial();
+            /* 
+            Las enumeraciones son un conjunto de valores subyasentes
+            que son valores int (enteros), uint (no enteros pero si naturales), short, etc...
+            para proveer orden en las consulta de la enumeracion o de la coleccion de 
+            valores que estan enumerados por su nombre. 
+
+            En una clase se puede crear una propiedad de tipo enumeracion para que contenga
+            el primer valor subyasente que este tega en la enumeracion si no se le asigna por el programador, pasa 
+            si se le asigna entonces empieza con el valor 0, esta variable solo 
+            acepta valores subyacentes de la coleccion en la cual se baso.
+
+            ejemplo: 
+            */
+            Beginning.Number = Subyasent.two;
+            // Beginning.Number = Subyasent.two;
+            Console.WriteLine((uint)Beginning.Number);
         }
     }
 }
